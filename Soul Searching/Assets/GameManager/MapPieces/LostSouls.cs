69f730233165b8cd283a351985e019586b9f5f94
dylanvandenhemel <60535253+dylanvandenhemel@users.noreply.github.com>
2021-10-18 08:56:50 -0500

    navmeshstuff

--- Soul Searching/Assets/GameManager/MapPieces/LostSouls.cs
+++ Soul Searching/Assets/GameManager/MapPieces/LostSouls.cs
@@ -1,14 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class LostSouls : MonoBehaviour
 {
+    private bool bCollected;
+
+    private Transform player;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
+            player = other.transform;
             Debug.Log("touched");
+            bCollected = true;
+        }
+    }
+
+    private void Update()
+    {
+        if(bCollected)
+        {
+            GetComponent<NavMeshAgent>().SetDestination(new Vector3(player.position.x, player.position.y, player.position.z - 0.5f));
         }
     }
 }
