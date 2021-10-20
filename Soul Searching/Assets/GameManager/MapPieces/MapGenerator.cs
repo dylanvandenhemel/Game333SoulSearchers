d0a940c98aafe96dc7e6e3611a1802574a6927be
dylanvandenhemel <60535253+dylanvandenhemel@users.noreply.github.com>
2021-10-20 14:40:36 -0500

    buffed Map builder

--- Soul Searching/Assets/GameManager/MapPieces/MapGenerator.cs
+++ Soul Searching/Assets/GameManager/MapPieces/MapGenerator.cs
@@ -5,8 +5,6 @@ using System;
 #if UNITY_EDITOR
 using UnityEditor;
 [ExecuteInEditMode]
-
-
 public class MapGenerator : MonoBehaviour
 {
     public bool mGenerate;
@@ -15,29 +13,42 @@ public class MapGenerator : MonoBehaviour
     private int counter;
     public List<GameObject> origTile;
     public bool bAutoPillar;
+
     public void Update()
     {
         if(mGenerate)
         {
             mGenerate = false;
+
+            //fills array with current tiles
             mapChildCount = transform.childCount;
             origTile.Clear();
             for(counter = 0; counter < mapChildCount; counter++)
             {
                 origTile.Add(transform.GetChild(counter).gameObject);
+                
             }
-
+      
+            //Activates each tiles map piece
             for (counter = 0; counter < mapChildCount; counter++)
             {
-                transform.GetChild(counter).GetComponent<Map>().AutoMap();
+                if (transform.GetChild(counter).GetComponent<Map>() != null)
+                {
+                    transform.GetChild(counter).GetComponent<Map>().AutoMap();
+                }
             }          
-
+            //Deletes old tiles
             for (counter = 0; counter < origTile.Count; counter++)
             {
-                DestroyImmediate(origTile[counter]);
+                if (transform.GetChild(counter).GetComponent<Map>() != null)
+                {
+                    DestroyImmediate(origTile[counter]);
+                }
+
             }
         }
 
+        //places pillers in needed corners
         if (bAutoPillar)
         {
             for (counter = 0; counter < mapChildCount; counter++)
