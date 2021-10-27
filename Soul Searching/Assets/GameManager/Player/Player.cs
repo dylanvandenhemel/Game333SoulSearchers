327a602bd01185a4dd71b961ff9ccb46f83d5935
dylanvandenhemel <60535253+dylanvandenhemel@users.noreply.github.com>
2021-10-27 08:58:27 -0500

    fixed pile of bones

--- Soul Searching/Assets/GameManager/Player/Player.cs
+++ Soul Searching/Assets/GameManager/Player/Player.cs
@@ -238,7 +238,7 @@ public class Player : MonoBehaviour
         bpossessSkel = false;
         transform.GetChild(0).GetComponent<MeshRenderer>().enabled = true;
         //moves pile back a bit after a trap
-        //currentSkeletonPile.localPosition = new Vector3(currentSkeletonPile.localPosition.x, currentSkeletonPile.localPosition.y - 0.5f, currentSkeletonPile.localPosition.z - 1);
+        currentSkeletonPile.localPosition = new Vector3(currentSkeletonPile.localPosition.x, currentSkeletonPile.localPosition.y, currentSkeletonPile.localPosition.z - 1);
 
         currentSkeletonPile.parent = null;
         pActions.PlayerActions.Possess.performed -= Possess;
