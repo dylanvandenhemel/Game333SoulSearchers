5d2bdbee3818764aef1a5d5df5524b0209042d5c
dylanvandenhemel <60535253+dylanvandenhemel@users.noreply.github.com>
2021-11-01 09:29:41 -0500

    collectableBody

--- Soul Searching/Assets/GameManager/MapPieces/LostSouls.cs
+++ Soul Searching/Assets/GameManager/MapPieces/LostSouls.cs
@@ -7,18 +7,51 @@ public class LostSouls : MonoBehaviour
 { 
     private bool bCollected;
 
+    public GameObject babySkel;
+
     private Transform player;
     private bool bFollow;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
-        if(other.CompareTag("Player") && !bFollow)
+        if(other.CompareTag("Player"))
         {
             player = other.transform;
             bCollected = true;
-            bFollow = true;
 
+            if(!bFollow)
+            {
+                if (player.GetComponent<Player>().bpossessSkel)
+                {
+                    transform.GetChild(0).gameObject.SetActive(false);
+                    babySkel.SetActive(true);
+                }
+                else
+                {
+                    transform.GetChild(0).gameObject.SetActive(true);
+                    babySkel.SetActive(false);
+                }
+            }
+
+            if (GetComponent<NavMeshAgent>().remainingDistance > 0.5f)
+            {
+                babySkel.GetComponent<Animator>().SetBool("bSkelWalk", true);
+            }
+            else
+            {
+                babySkel.GetComponent<Animator>().SetBool("bSkelWalk", false);
+            }
+            
             player.GetComponent<Player>().collectablesCount++;
+            bFollow = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            bFollow = false;
         }
     }
 
---
+++ Soul Searching/Assets/GameManager/MapPieces/MapWindow.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEditor;
+
+public class MapWindow : EditorWindow
+{
+    /*
+    private bool bToggle;
+    private int mapCounter;
+    private float mapFlaot;
+    private string mapWindowText;
+    private Transform mapGenerator;
+    [MenuItem("Map Generator/Generator")]
+
+
+    public static void ShowWindow()
+    {
+        GetWindow(typeof(MapWindow));
+    }
+
+    private void OnGUI()
+    {
+        bToggle = EditorGUILayout.Toggle("boolean", bToggle);
+        mapCounter = EditorGUILayout.IntField("mapInteger", mapCounter);
+        mapFlaot = EditorGUILayout.FloatField("mapFloat", mapFlaot);
+        mapWindowText = EditorGUILayout.TextField("mapText", mapWindowText);
+        mapGenerator = EditorGUILayout.ObjectField("mapTransform", mapGenerator, typeof(Transform), true)as Transform;
+    }
+    */
+
+
+
+}
