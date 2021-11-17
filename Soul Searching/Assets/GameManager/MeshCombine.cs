430c6f36a7c466188b87a11728f482ff508844d7
dylanvandenhemel <60535253+dylanvandenhemel@users.noreply.github.com>
2021-11-17 08:54:21 -0600

    optimization also doorway gate fix

--- Soul Searching/Assets/GameManager/MeshCombine.cs
+++ Soul Searching/Assets/GameManager/MeshCombine.cs
@@ -6,20 +6,25 @@ using UnityEngine;
 [RequireComponent(typeof(MeshRenderer))]
 public class MeshCombine : MonoBehaviour
 {
+    Vector3 startPos;
     void Start()
     {
         MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
         CombineInstance[] combine = new CombineInstance[meshFilters.Length];
 
-        int i = 0;
-        while (i < meshFilters.Length)
+        startPos = transform.position;
+        transform.position = Vector3.zero;
+
+        for(int i = 0; i < meshFilters.Length; i++)
         {
             combine[i].mesh = meshFilters[i].sharedMesh;
             combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
-            meshFilters[i].gameObject.SetActive(false);
 
-            i++;
+            meshFilters[i].gameObject.SetActive(false);
         }
+
+        transform.position = startPos;
+
         transform.GetComponent<MeshFilter>().mesh = new Mesh();
         transform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine);
         transform.gameObject.SetActive(true);
