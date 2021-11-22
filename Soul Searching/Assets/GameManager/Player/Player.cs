4c51e536f956e642eebeb3b116aa61c412ade6c5
dylanvandenhemel <60535253+dylanvandenhemel@users.noreply.github.com>
2021-11-22 17:56:25 -0600

    silver door fixed, dog adjusted

--- Soul Searching/Assets/GameManager/Player/Player.cs
+++ Soul Searching/Assets/GameManager/Player/Player.cs
@@ -300,7 +300,7 @@ public class Player : MonoBehaviour
     {
         bwhistling = true;
         GetComponent<PlayerSound>().PlayerWistle();
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.4f);
         bwhistling = false;
     }
 
