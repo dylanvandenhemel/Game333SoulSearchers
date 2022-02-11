06397f9cbb682c819ff34d3e60d848ba586bebf4
Spencersxtn <33520072+Spencersxtn@users.noreply.github.com>
2022-02-11 08:09:12 -0600

    Particle works
    Currently, it is setup for one particle at a time. Each one will finish movement before the next spawns.

--- Soul Searching/Assets/GameManager/ParticleMovement.cs
+++ Soul Searching/Assets/GameManager/ParticleMovement.cs
@@ -6,6 +6,7 @@ public class ParticleMovement : MonoBehaviour
 {
     public float speed = .1f;
     [HideInInspector] public Transform destination;
+    [HideInInspector] public List<GameObject> particleList;
 
     // Update is called once per frame
     void Update()
@@ -13,6 +14,13 @@ public class ParticleMovement : MonoBehaviour
         if (Vector3.Distance(transform.position, destination.position) >= speed)
             transform.position = Vector3.MoveTowards(transform.position, destination.position, speed);
         else
+        {
+            foreach (GameObject particle in particleList)
+            {
+                if (particle == gameObject)
+                    particleList.Remove(particle);
+            }
             Destroy(gameObject);
+        }
     }
 }
