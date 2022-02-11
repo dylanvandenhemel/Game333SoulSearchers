d5f9b441b1682cff836061f3a4f69e0955321401
dylanvandenhemel <60535253+dylanvandenhemel@users.noreply.github.com>
2022-02-11 08:43:04 -0600

    Revert "Particle works"
    This reverts commit 06397f9cbb682c819ff34d3e60d848ba586bebf4.

--- Soul Searching/Assets/GameManager/ParticleMovement.cs
+++ Soul Searching/Assets/GameManager/ParticleMovement.cs
@@ -6,7 +6,6 @@ public class ParticleMovement : MonoBehaviour
 {
     public float speed = .1f;
     [HideInInspector] public Transform destination;
-    [HideInInspector] public List<GameObject> particleList;
 
     // Update is called once per frame
     void Update()
@@ -14,13 +13,6 @@ public class ParticleMovement : MonoBehaviour
         if (Vector3.Distance(transform.position, destination.position) >= speed)
             transform.position = Vector3.MoveTowards(transform.position, destination.position, speed);
         else
-        {
-            foreach (GameObject particle in particleList)
-            {
-                if (particle == gameObject)
-                    particleList.Remove(particle);
-            }
             Destroy(gameObject);
-        }
     }
 }
